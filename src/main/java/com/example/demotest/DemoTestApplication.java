0d440eec10e93174d5a80e5fa510aea0e7be45a8
ullasdasprogramm <61796466+ullasdasprogramm@users.noreply.github.com>
2022-12-18 20:14:57 +0530

    Update DemoTestApplication.java

--- src/main/java/com/example/demotest/DemoTestApplication.java
+++ src/main/java/com/example/demotest/DemoTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoTestApplication.class, args);
+		SpringApplication.run(DemoTestApplication.class, args)
 	}
 
 }
